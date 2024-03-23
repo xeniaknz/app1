7c893b42597002580c0bc530e1eab26b49ac7651
xeniaknz <107608965+xeniaknz@users.noreply.github.com>
2024-03-23 12:16:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ with col1:
     4) kopeeri ükshaaval tarbijasegmentide lõikes äpi väljundteksti kõnealuse toote tutvustuslehele.")
 
 with col2:
-    st.image(image='companylogo.jpg', caption='Natural and healthy shirts for everybody')
+    st.image(image='pocologo.png', caption='Natural and healthy shirts for everybody')
 
 st.markdown("## Enter Your Content To Convert")
 
